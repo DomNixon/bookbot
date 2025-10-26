9bdc080f759df66eb9435c9e5f69d7188456f02e
DomNixon <dominicnixon123@gmail.com>
2025-10-26 18:05:03 -0400

    Bookbot done, now adding arguments

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from stats import num_words
 from stats import num_char
+from stats import sorted_alpha_num_char
 
 def book_capture():
 	with open("books/frankenstein.txt") as f:
 		file_content = f.read()
 		return file_content
 
-
 def main ():
+	book_text = book_capture()
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {num_words(book_text)} total words")
+	print("--------- Character Count -------")
+	counts = num_char(book_text)
+	sorted_alpha_num_char(counts)
+	print("============= END ===============")
 
-	print(f"Found {num_words(book_capture())} total words")
-	print(num_char(book_capture()))
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,24 @@
 def num_words(text):
 	word_list = text.split()
-	total = len(word_list)
-	return total
+	return len(word_list)
 
 
 def num_char(text):
 	char_count = {}
 	ltext = text.lower()
 	for char in ltext:
-		if char in char_count:
-			char_count[char] += 1
-		else:
-			char_count[char] = 1
+		if char.isalpha():
+			if char in char_count:
+				char_count[char] += 1
+			else:
+				char_count[char] = 1
 	return char_count
+
+def sorted_alpha_num_char(text):
+	sorted_items = sorted(
+        text.items(),
+        key=lambda item: item[1],
+        reverse=True
+    )
+	for char, num in sorted_items:
+			print(f"{char}: {num}")
