6944fed4005d226728351cd96050e58b67450c74
DomNixon <dominicnixon123@gmail.com>
2025-10-26 18:21:10 -0400

    Finished bookbot, with args

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import sys
 from stats import num_words
 from stats import num_char
 from stats import sorted_alpha_num_char
 
+try:
+	book_location = sys.argv[1]
+	print(book_location)
+except IndexError:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+
 def book_capture():
-	with open("books/frankenstein.txt") as f:
+	with open(book_location) as f:
 		file_content = f.read()
 		return file_content
 
