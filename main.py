efbc7ab5f569ad53fa69d34e1a1b9954c9b0276d
DomNixon <dominicnixon123@gmail.com>
2025-10-25 23:01:23 -0400

    CH2L6 Counting Characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import num_words
+from stats import num_char
 
 def book_capture():
 	with open("books/frankenstein.txt") as f:
@@ -9,5 +10,5 @@ def book_capture():
 def main ():
 
 	print(f"Found {num_words(book_capture())} total words")
-
+	print(num_char(book_capture()))
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def num_words(text):
 	word_list = text.split()
 	total = len(word_list)
 	return total
+
+
+def num_char(text):
+	char_count = {}
+	ltext = text.lower()
+	for char in ltext:
+		if char in char_count:
+			char_count[char] += 1
+		else:
+			char_count[char] = 1
+	return char_count
